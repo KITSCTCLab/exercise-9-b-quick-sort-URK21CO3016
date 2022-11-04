2e247bab201bf28ed6b4d90f84a0d5e5af541079
URK21CO3016 <110014964+URK21CO3016@users.noreply.github.com>
2022-11-04 22:08:49 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,45 +1,22 @@
 from typing import List
 
-def partition(array, low, high):
+def quick_sort(data, low, high) -> List[int]:
+    # Write code here
+    if low < high:
+        p = partition(data, low, high)
+        data = quick_sort(data, low, p - 1)
+        data = quick_sort(data, p + 1, high)
+    return data
 
-  # choose the rightmost element as pivot
-  pivot = array[high]
-
-  # pointer for greater element
-  i = low - 1
-
-  # traverse through all elements
-  # compare each element with pivot
-  for j in range(low, high):
-    if array[j] <= pivot:
-      # if element smaller than pivot is found
-      # swap it with the greater element pointed by i
-      i = i + 1
-
-      # swapping element at i with element at j
-      (array[i], array[j]) = (array[j], array[i])
-
-  # swap the pivot element with the greater element specified by i
-  (array[i + 1], array[high]) = (array[high], array[i + 1])
-
-  # return the position from where partition is done
-  return i + 1
-
-# function to perform quicksort
-def quickSort(array, low, high):
-  if low < high:
-
-    # find pivot element such that
-    # element smaller than pivot are on the left
-    # element greater than pivot are on the right
-    pi = partition(array, low, high)
-
-    # recursive call on the left of pivot
-    quickSort(array, low, pi - 1)
-
-    # recursive call on the right of pivot
-    quickSort(array, pi + 1, high)
-
+def partition(data, low, high):
+    i = low - 1
+    for j in range(low, high):
+        if data[j] < data[high]:
+            i += 1
+            data[i], data[j] = data[j], data[i]
+    data[i + 1], data[high] = data[high], data[i + 1]
+    return i + 1
+    
 
 
 input_data = input()
